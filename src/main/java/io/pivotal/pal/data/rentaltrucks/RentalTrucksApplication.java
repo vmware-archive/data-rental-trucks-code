53707e0fa955f65632b9676f6b241db0f51f122d
David Wadden <dwadden@pivotal.io>
2018-04-23 14:00:31 -0500

    Lints code for various code conventions and renames

---
+++ src/main/java/io/pivotal/pal/data/rentaltrucks/RentalTrucksApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.pal.data.rentaltrucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalTrucksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalTrucksApplication.class, args);
+    }
+}
--- src/test/java/io/pivotal/pal/data/rentaltrucks/RentalTrucksApplicationTest.java
+++ src/test/java/io/pivotal/pal/data/rentaltrucks/RentalTrucksApplicationTest.java
@@ -1,4 +1,4 @@
-package io.pivotal.pal.data.datarentaltruckscode;
+package io.pivotal.pal.data.rentaltrucks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DataRentalTrucksCodeApplicationTests {
+public class RentalTrucksApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
