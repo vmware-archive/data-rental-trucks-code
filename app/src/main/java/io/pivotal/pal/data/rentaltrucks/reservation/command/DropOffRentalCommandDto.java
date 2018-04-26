ff3e64fcff11800144e9f114da70295268f2e801
David Wadden <dwadden@pivotal.io>
2018-04-26 15:57:59 -0500

    Looks up truckVin during rental dropoff
    Signed-off-by: Adam Neumann <aneumann@pivotal.io>

--- app/src/main/java/io/pivotal/pal/data/rentaltrucks/reservation/command/DropOffRentalCommandDto.java
+++ app/src/main/java/io/pivotal/pal/data/rentaltrucks/reservation/command/DropOffRentalCommandDto.java
@@ -8,24 +8,17 @@ import java.util.Objects;
 public class DropOffRentalCommandDto {
 
     private final String confirmationNumber;
-    private final String truckVin;
     private final Integer dropOffMileage;
 
     @JsonCreator
     public DropOffRentalCommandDto(@JsonProperty("confirmationNumber") String confirmationNumber,
-                                   @JsonProperty("truckVin") String truckVin,
                                    @JsonProperty("dropOffMileage") Integer dropOffMileage) {
         this.confirmationNumber = confirmationNumber;
-        this.truckVin = truckVin;
         this.dropOffMileage = dropOffMileage;
     }
 
     public String getConfirmationNumber() {
         return confirmationNumber;
-    }
-
-    public String getTruckVin() {
-        return truckVin;
     }
 
     public Integer getDropOffMileage() {
@@ -38,20 +31,18 @@ public class DropOffRentalCommandDto {
         if (o == null || getClass() != o.getClass()) return false;
         DropOffRentalCommandDto that = (DropOffRentalCommandDto) o;
         return Objects.equals(confirmationNumber, that.confirmationNumber) &&
-                Objects.equals(truckVin, that.truckVin) &&
                 Objects.equals(dropOffMileage, that.dropOffMileage);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(confirmationNumber, truckVin, dropOffMileage);
+        return Objects.hash(confirmationNumber, dropOffMileage);
     }
 
     @Override
     public String toString() {
         return "DropOffRentalCommandDto{" +
                 "confirmationNumber='" + confirmationNumber + '\'' +
-                ", truckVin='" + truckVin + '\'' +
                 ", dropOffMileage=" + dropOffMileage +
                 '}';
     }
--- app/src/main/java/io/pivotal/pal/data/rentaltrucks/reservation/command/DropOffRentalCommandService.java
+++ app/src/main/java/io/pivotal/pal/data/rentaltrucks/reservation/command/DropOffRentalCommandService.java
@@ -2,24 +2,30 @@ package io.pivotal.pal.data.rentaltrucks.reservation.command;
 
 import io.pivotal.pal.data.rentaltruck.framework.event.AsyncEventPublisher;
 import io.pivotal.pal.data.rentaltrucks.event.TruckDroppedOffEvent;
+import io.pivotal.pal.data.rentaltrucks.reservation.domain.Rental;
+import io.pivotal.pal.data.rentaltrucks.reservation.domain.RentalRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DropOffRentalCommandService {
 
+    private final RentalRepository rentalRepository;
     private final AsyncEventPublisher<TruckDroppedOffEvent> eventPublisher;
 
-    public DropOffRentalCommandService(AsyncEventPublisher<TruckDroppedOffEvent> eventPublisher) {
+    public DropOffRentalCommandService(RentalRepository rentalRepository,
+                                       AsyncEventPublisher<TruckDroppedOffEvent> eventPublisher) {
+        this.rentalRepository = rentalRepository;
         this.eventPublisher = eventPublisher;
     }
 
     public void dropOffRental(DropOffRentalCommandDto commandDto) {
 
-        // TODO: Do we need the truck VIN from the command DTO or can we derive it here?
+        // find truck vin by looking up rental
+        Rental rental = rentalRepository.findOne(commandDto.getConfirmationNumber());
 
         // emit truck dropped off event
         TruckDroppedOffEvent event =
-                new TruckDroppedOffEvent(commandDto.getConfirmationNumber(), commandDto.getTruckVin(), commandDto.getDropOffMileage());
+                new TruckDroppedOffEvent(commandDto.getConfirmationNumber(), rental.getTruckVin(), commandDto.getDropOffMileage());
         eventPublisher.publish(event);
     }
 
